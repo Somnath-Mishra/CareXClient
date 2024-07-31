d9cfe84ab137733cfc249a3d03f1855cdab5bff7
Somnath Mishra <somnath012003@gmail.com>
2024-07-31 17:20:23 +0530

    changing backend url

--- src/conf/conf.ts
+++ src/conf/conf.ts
@@ -1,4 +1,5 @@
 export const conf={
     razorPayKeyId:String(import.meta.env.VITE_RAZORPAY_KEY_ID),
-    stripePublicApiKey: String(import.meta.env.VITE_STRIPE_PUBLIC_API_KEY)
+    stripePublicApiKey: String(import.meta.env.VITE_STRIPE_PUBLIC_API_KEY),
+    serverUrl: String(import.meta.env.VITE_SERVER_URL)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
-      '/api':'https://w3xoeef8x8.execute-api.ap-south-1.amazonaws.com/production',
+      '/api': 'https://txzazouq7a.execute-api.ap-south-1.amazonaws.com/production',
     }
   }
 })
+
+
+
+
