02669409c1e11337d3a63fafc92bb1010a805d0e
Somnath Mishra <somnath012003@gmail.com>
2024-08-15 20:12:15 +0530

    fixing source

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    proxy:{
-      '/api': 'https://txzazouq7a.execute-api.ap-south-1.amazonaws.com/production',
-      // '/api':'http://localhost:9000'
+    // proxy:{
+    //   '/api': 'https://txzazouq7a.execute-api.ap-south-1.amazonaws.com/production',
+    //   // '/api':'http://localhost:9000'
+    // }
+    proxy: {
+      '/api': {
+        target: 'https://txzazouq7a.execute-api.ap-south-1.amazonaws.com/production',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
     }
   }
 })
